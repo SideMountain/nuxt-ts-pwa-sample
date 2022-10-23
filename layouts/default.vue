3bd97f939f40b432f4904e03c247b561d7be49d8
yuya.yokoyama <yuya.yokoyama@monocrea.co.jp>
2022-10-23 20:45:51 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-main id="main">
+    <v-app>
+      <app-header />
+      <v-container>
+        <nuxt />
+      </v-container>
+      <app-footer class="mt-auto" />
+    </v-app>
+  </v-main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Default extends Vue {}
+</script>
